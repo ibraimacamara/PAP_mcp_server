e00eb9428d8ac1f20c86ebb6810304720f4de917
camaraibraima <camaraibraima5580@gmail.com>
2026-04-22 15:20:25 +0100

    altereção de cabeçalho de site

--- dashmin/admin/menu.php
+++ dashmin/admin/menu.php
@@ -40,7 +40,7 @@ $fotoPath = file_exists(__DIR__ . '/../uploads/' . $fotoUser)
 
 <head>
     <meta charset="utf-8">
-    <title>DASHMIN - Bootstrap Admin Template</title>
+    <title>SGEI</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
